5967745556a77feffa80aaf0979735fe4ed0fef0
kimkh.james@gmail.com <kimkh.james@gmail.com>
2018-11-30 07:53:04 -0500

    merge conflict resolved2

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def main(msg):
-<<<<<<< HEAD
+
     //different 2nd comment to this file
-=======
     // print a message
->>>>>>> refs/remotes/origin/master
     print(msg)
 
 main("hello world")
